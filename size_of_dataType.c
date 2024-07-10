c746b73fd96b14f9c558320c5c021ba9a465e6dd
Deekshith M <156458393+deekshith0803@users.noreply.github.com>
2024-07-10 20:36:19 +0530

    Add files via upload

---
+++ size_of_dataType.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Print the size of various data types
+    printf("Size of char: %lu byte\n", sizeof(char));
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of float: %lu bytes\n", sizeof(float));
+    printf("Size of double: %lu bytes\n", sizeof(double));
+    printf("Size of short: %lu bytes\n", sizeof(short));
+    printf("Size of long: %lu bytes\n", sizeof(long));
+    printf("Size of long long: %lu bytes\n", sizeof(long long));
+    
+    return 0;
+}
