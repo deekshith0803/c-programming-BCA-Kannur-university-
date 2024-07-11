e89ec745a856e5b6770cc2ffd4bc547eb85b284b
Deekshith M <156458393+deekshith0803@users.noreply.github.com>
2024-07-11 15:26:30 +0530

    Update size_of_dataType.c

--- size_of_dataType.c
+++ size_of_dataType.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
+#include <limits.h>
+#include <float.h>
 
 int main() {
-    // Print the size of various data types
-    printf("Size of char: %lu byte\n", sizeof(char));
-    printf("Size of int: %lu bytes\n", sizeof(int));
-    printf("Size of float: %lu bytes\n", sizeof(float));
-    printf("Size of double: %lu bytes\n", sizeof(double));
-    printf("Size of short: %lu bytes\n", sizeof(short));
-    printf("Size of long: %lu bytes\n", sizeof(long));
-    printf("Size of long long: %lu bytes\n", sizeof(long long));
-    
+    // char
+    printf("Data type: char\n");
+    printf("Size: %zu bytes\n", sizeof(char));
+    printf("Range: %d to %d\n\n", CHAR_MIN, CHAR_MAX);
+
+    // int
+    printf("Data type: int\n");
+    printf("Size: %zu bytes\n", sizeof(int));
+    printf("Range: %d to %d\n\n", INT_MIN, INT_MAX);
+
+    // float
+    printf("Data type: float\n");
+    printf("Size: %zu bytes\n", sizeof(float));
+    printf("Range: %.10e to %.10e\n\n", FLT_MIN, FLT_MAX);
+
+    // double
+    printf("Data type: double\n");
+    printf("Size: %zu bytes\n", sizeof(double));
+    printf("Range: %.10e to %.10e\n\n", DBL_MIN, DBL_MAX);
+
+    // long
+    printf("Data type: long\n");
+    printf("Size: %zu bytes\n", sizeof(long));
+    printf("Range: %ld to %ld\n\n", LONG_MIN, LONG_MAX);
+
     return 0;
 }
